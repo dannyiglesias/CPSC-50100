0b387de47c276c1d62d808ea53a0015036e8e8a2
dannyiglesias <dannyiglesias@gmail.com>
2018-06-05 12:15:34 -0400

    Update elevators.java

--- elevators.java
+++ elevators.java
@@ -12,21 +12,15 @@ public class elevators3 {
 		String[] eTravel = {"Traveling Down 2 Floors", "Traveling Down 1 Floor", "Staying on this Floor", "Traveling Up 1 Floor", "Traveling Up 2 Floors"};
 		int elevatorFloor = elevatorArray[0];
 		String DistanceTraveled = null;
-		//elevatorFloor = elevatorArray[0];
 		Random generator = new Random();//Random floor request generator
 		int randomButtonPush = 0;
 		
 		for(int i=0; i<10; i++) {
-			//elevatorFloor = elevatorArray[0];
-			//Random generator = new Random();//Random floor request generator
-			//int randomButtonPush = generator.nextInt(pushArray.length);//Selects Random floor from pushArray
-			while(elevatorArray[i]==elevatorArray[0]) {
+			while(elevatorFloor==0) {
 				randomButtonPush = generator.nextInt(pushArray.length);
 				if(randomButtonPush==0) {
 					DistanceTraveled = eTravel[2];
-					//elevatorFloor=0;
-					elevatorArray[i] = elevatorArray[0];
-					elevatorFloor = elevatorArray[i];
+					elevatorFloor = 0;
 					System.out.println("Elevator is located on the "+floorArray[0] + "\nThe Elevator will be "+ DistanceTraveled);
 					try {
 						//wait 2 seconds
@@ -38,9 +32,7 @@ public class elevators3 {
 				}
 				else if(randomButtonPush==1) {
 					DistanceTraveled = eTravel[3];
-					//elevatorFloor=1;
-					elevatorArray[i] = elevatorArray[i++];
-					elevatorFloor = elevatorArray[i];
+					elevatorFloor = 1;
 					System.out.println("Elevator is located on the "+floorArray[0] + "\nThe Elevator will be " + DistanceTraveled + " to the " + floorArray[1]);
 					try {
 						//wait 2 seconds
@@ -61,9 +53,7 @@ public class elevators3 {
 				}
 				else if(randomButtonPush==2) {
 					DistanceTraveled = eTravel[4];
-					//elevatorFloor=2;
-					elevatorArray[i] = elevatorArray[i+2];
-					elevatorFloor = elevatorArray[i];
+					elevatorFloor = 2;
 					System.out.println("Elevator is located on the "+ floorArray[0] + "\nThe Elevator will be " + DistanceTraveled + " to the " + floorArray[2]);
 					try {
 						//wait 4 seconds
@@ -83,13 +73,11 @@ public class elevators3 {
 				}
 			
 			}
-			while(elevatorArray[i]==elevatorArray[1]) {
+			while(elevatorFloor==1) {
 				randomButtonPush = generator.nextInt(pushArray.length);
 				if(randomButtonPush==0) {
 					DistanceTraveled = eTravel[1];
-					//elevatorFloor=0;
-					elevatorArray[i] = elevatorArray[i+2];
-					elevatorFloor = elevatorArray[i];
+					elevatorFloor = 0;
 					System.out.println("Elevator is located on the "+floorArray[1] + "\nThe Elevator will be "+ DistanceTraveled + " to the " + floorArray[0]);
 					try {
 						//wait 2 seconds
@@ -109,8 +97,7 @@ public class elevators3 {
 				}
 				else if(randomButtonPush==1) {
 					DistanceTraveled = eTravel[2];
-					//elevatorFloor=1;
-					elevatorFloor = elevatorArray[i];
+					elevatorFloor = 1;
 					System.out.println("Elevator is located on the "+floorArray[1] + "\nThe Elevator will be " + DistanceTraveled);
 					try {
 						//wait 2 seconds
@@ -124,9 +111,7 @@ public class elevators3 {
 				}
 				else if(randomButtonPush==2) {
 					DistanceTraveled = eTravel[3];
-					//elevatorFloor=2;
-					elevatorArray[i] = elevatorArray[i++];
-					elevatorFloor = elevatorArray[i];
+					elevatorFloor = 2;
 					System.out.println("Elevator is located on the "+ floorArray[1] + "\nThe Elevator will be " + DistanceTraveled + " to the " + floorArray[2]);
 					try {
 						//wait 2 seconds
@@ -147,13 +132,11 @@ public class elevators3 {
 				
 			
 			}
-			while(elevatorArray[i]==elevatorArray[2]) {
+			while(elevatorFloor==2) {
 				randomButtonPush = generator.nextInt(pushArray.length);
 				if(randomButtonPush==0) {
 					DistanceTraveled = eTravel[0];
-					//elevatorFloor=0;
-					elevatorArray[i] = elevatorArray[i++];
-					elevatorFloor = elevatorArray[i];
+					elevatorFloor = 0;
 					System.out.println("Elevator is located on the "+floorArray[2] + "\nThe Elevator will be "+ DistanceTraveled + " to the " + floorArray[0]);
 					try {
 						//wait 4 seconds
@@ -166,9 +149,7 @@ public class elevators3 {
 				}
 				else if(randomButtonPush==1) {
 					DistanceTraveled = eTravel[1];
-					//elevatorFloor=1;
-					elevatorArray[i] = elevatorArray[i+2];
-					elevatorFloor = elevatorArray[i];
+					elevatorFloor = 1;
 					System.out.println("Elevator is located on the "+floorArray[2] + "\nThe Elevator will be " + DistanceTraveled + " to the " + floorArray[1]);
 					try {
 						//wait 2 seconds
@@ -182,8 +163,7 @@ public class elevators3 {
 				}
 				else if(randomButtonPush==2) {
 					DistanceTraveled = eTravel[2];
-					//elevatorFloor=2;
-					elevatorFloor = elevatorArray[i];
+					elevatorFloor = 2;
 					System.out.println("Elevator is located on the "+ floorArray[2] + "\nThe Elevator will be " + DistanceTraveled);
 					try {
 						//wait 2 seconds
@@ -202,10 +182,3 @@ public class elevators3 {
 	}
 
 }
-
-		
-				
-		
-	
-
-
